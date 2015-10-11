73faf25816da1fe64272db8237d0b8c46c990be5
Fausto <fausto@debian>
2015-10-11 22:07:34 +0200

    Aggiornato il file prenotazioni.html e il file codice.js per quanto riguarda il controllo della scelta della struttura.

--- progetto/codici/codice.js
+++ progetto/codici/codice.js
@@ -1,9 +1,27 @@
-function avanti_su_periodo() {
-	location.href="prenotazioni_periodo.html";
-	alert("Prova superata");
+//Eseguo il controllo che venga effettuata una scelta della struttura.
+//Se manca la scelta si ripresenta la pagina di inserimento.
+function controllo_strutture() {
+	if(document.scelta_struttura.struttura.value=="" || document.scelta_struttura.struttura.value==null) {
+	alert("Effettua la scelta della struttura");
+	return false;
+	}
+	else return true;
+
 }
+
+
 
 function indietro() {
 	history.back();
-	alert("Prova riuscita");
+	alert("Prova non superata");
+}
+
+function avanti_su_anagrafica() {
+	location.href="prenotazioni_anagrafica.html";
+	alert("Prova non superata");
+}
+
+function indietro_a() {
+	location.href="prenotazioni_periodo.html";
+	alert("Prova non superata");
 }
