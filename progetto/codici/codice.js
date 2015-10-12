d4269cdfde7c47a7c8c811dc6e3d9629df34d0e7
Fausto <fausto@debian>
2015-10-12 23:00:07 +0200

    Aggiornato il file prenotazioni.html e il file codice.js per quanto riguarda il controllo della scelta della struttura.

--- progetto/codici/codice.js
+++ progetto/codici/codice.js
@@ -1,11 +1,33 @@
 //Eseguo il controllo che venga effettuata una scelta della struttura.
 //Se manca la scelta si ripresenta la pagina di inserimento.
 function controllo_strutture() {
-	if(document.scelta_struttura.struttura.value=="" || document.scelta_struttura.struttura.value==null) {
+//	if(document.scelta_struttura.struttura.value=="" || document.scelta_struttura.struttura.value==null) 
+
+	switch(true) {
+		case document.getElementsByName("struttura")[0].checked:
+			alert("Pisani");
+			return true;
+		case document.getElementsByName("struttura")[1].checked:
+			alert("Palladio");
+			return true;
+		case document.getElementsByName("struttura")[2].checked:
+			alert("Rotonda");
+			return true;
+		default:
+			alert("Effettua una scelta");
+			return false;
+	}
+
+/*
+	if(document.getElementByName("struttura")=="" || document.getElementByName("struttura")==null) {
 	alert("Effettua la scelta della struttura");
 	return false;
 	}
-	else return true;
+	else {
+	alert(document.getElementByName("struttura".value));
+ 	return true;
+	}
+*/
 
 }
 
