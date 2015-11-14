c0543152c5b84056b2d2bd26f5ee6da88bc02628
Fausto <fausto@debian>
2015-11-14 19:37:09 +0100

    corretti alcunierrori minori

--- progetto/trasferimento.bash
+++ progetto/trasferimento.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -p 4LfdGKh% rsync --delete --exclude='locale/' --exclude='villaggio/' --exclude='*~' --exclude='*.bash' -azv -e "ssh -p 30022" ~/Documenti/Sistemi_Informativi/Esame/progetto/ fdalcaro@localhost:tecweb/public_html
+sshpass -p 5LfdGKh% rsync --delete --exclude='locale/' --exclude='villaggio/' --exclude='*~' --exclude='*.bash' -azv -e "ssh -p 30022" ~/Documenti/Sistemi_Informativi/Esame/progetto/ fdalcaro@localhost:tecweb/public_html
